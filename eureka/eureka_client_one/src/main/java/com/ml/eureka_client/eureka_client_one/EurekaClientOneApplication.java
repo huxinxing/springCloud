02385befa3d9148faa803650947ccaff5fe9e12d
huxinxing <huxinxings@aliyun.com>
2019-04-15 22:05:55 +0800

    a

---
+++ eureka/eureka_client_one/src/main/java/com/ml/eureka_client/eureka_client_one/EurekaClientOneApplication.java
@@ -0,0 +1,15 @@
+package com.ml.eureka_client.eureka_client_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientOneApplication.class, args);
+    }
+
+}
